ebb27b8d52b6c7bf7ec13fa03b806ce71a6c5c77
Hasitha Senanayaka <32666359+wshsenanayaka@users.noreply.github.com>
2019-08-13 20:09:52 +0530

    Delete check.php

--- include/check.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $email= $_SESSION['email'];
-    require '../include/config.php';
-    if(!isset($_SESSION['email'])){
-    header('Location: ../index.php');
-    }
-
